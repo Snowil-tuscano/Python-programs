95d1c69c1f6d710da5bb6d1fa846a40502567ebe
Snowil-tuscano <103951058+Snowil-tuscano@users.noreply.github.com>
2024-01-14 09:51:48 +0530

    first commit of python

---
+++ diff print.py
@@ -0,0 +1,8 @@
+print(" hello world ! ")
+q=10
+print(f" a is {q}")
+a=10
+b=20
+c=a+b
+print(f"{a} is first number and {b} is second number and addition is  {c}")
+print("{a} is good number ".format())
