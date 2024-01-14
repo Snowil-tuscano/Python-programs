946fbed06b240cecc1cc01a9262a08ad48a22811
Snowil-tuscano <103951058+Snowil-tuscano@users.noreply.github.com>
2024-01-14 19:39:42 +0530

    doen

--- diff_print.py
+++ diff_print.py
@@ -5,4 +5,11 @@ a=10
 b=20
 c=a+b
 print(f"{a} is first number and {b} is second number and addition is  {c}")
-print("{a} is good number ".format())
+print("{a} is good number ".format(a=10))
+
+def addition(a,b):
+     return a+b
+ 
+def multiply(a, b):
+ return a * b
+ 
---
+++ first letter .py
@@ -0,0 +1,11 @@
+# str = input("enter string ")
+
+# print(str)
+# if  str==str.upper() :
+#     print(" all are uppercase ")
+# else:
+#     print(" no uppercase")
+    
+import diff_print;    
+    
+print(diff_print.addition(10,20))
