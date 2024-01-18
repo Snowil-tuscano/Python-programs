abcb3e4a20c220ebd39f06410f23b14cc8647d9f
Snowil-tuscano <103951058+Snowil-tuscano@users.noreply.github.com>
2024-01-18 19:35:02 +0530

    new added

---
+++ prg1/p3.py
@@ -0,0 +1,5 @@
+import math
+num = int(input(" enter a  number to find the square root "))
+sqr = num**0.5
+print(f" square root of {num} is {math.floor(sqr)}")
+print(f" square root of the give number is {math.sqrt(num)}")
---
+++ prg1/p4.py
@@ -0,0 +1,5 @@
+len = int(input(" enter the length of the rectangle "))
+bre = int(input(" enter the Breath  of the rectangle "))
+
+area = len * bre
+print(f" area of the rectangle : {area}")
