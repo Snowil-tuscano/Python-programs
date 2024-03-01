88edd22d621b82218bbb215e5f195a086ffc1030
Snowil-tuscano <103951058+Snowil-tuscano@users.noreply.github.com>
2024-03-01 09:44:15 +0530

    day 3

---
+++ pr9/pr1.py
@@ -0,0 +1,7 @@
+my_dict = {'banana': 3, 'apple': 1, 'orange': 2}
+
+sorted_dict_keys = dict(sorted(my_dict.items()))
+
+
+print("Original dictionary:", my_dict)
+print("Sorted dictionary by keys:", sorted_dict_keys)
---
+++ pr9/pr2.py
@@ -0,0 +1,11 @@
+# Sample dictionaries
+dic1 = {1: 10, 2: 20}
+dic2 = {3: 30, 4: 40}
+dic3 = {5: 50, 6: 60}
+
+concatenated_dict = {}
+concatenated_dict.update(dic1)
+concatenated_dict.update(dic2)
+concatenated_dict.update(dic3)
+
+print("Concatenated dictionary:", concatenated_dict)
