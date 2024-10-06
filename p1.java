2553fab9dae0c47ea89b5d3af065f1a5d9aa129e
Snowil Tuscano <103951058+Snowil-tuscano@users.noreply.github.com>
2024-10-07 04:44:42 +0530

    Create p1.java

---
+++ p1.java
@@ -0,0 +1,80 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+class SharedQueue {
+    private Queue<Integer> queue = new LinkedList<>();
+    private int capacity;
+
+    public SharedQueue(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public synchronized void produce(int item) throws InterruptedException {
+        while (queue.size() == capacity) {
+            wait();
+        }
+        queue.add(item);
+        System.out.println("Produced: " + item);
+        notify();
+    }
+
+    public synchronized void consume() throws InterruptedException {
+        while (queue.isEmpty()) {
+            wait();
+        }
+        int item = queue.remove();
+        System.out.println("Consumed: " + item);
+        notify();
+    }
+}
+
+class Producer implements Runnable {
+    private SharedQueue sharedQueue;
+
+    public Producer(SharedQueue sharedQueue) {
+        this.sharedQueue = sharedQueue;
+    }
+
+    public void run() {
+        try {
+            while (true) {
+                int item = (int) (Math.random() * 100);
+                sharedQueue.produce(item);
+                Thread.sleep(500);
+            }
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+}
+
+class Consumer implements Runnable {
+    private SharedQueue sharedQueue;
+
+    public Consumer(SharedQueue sharedQueue) {
+        this.sharedQueue = sharedQueue;
+    }
+
+    public void run() {
+        try {
+            while (true) {
+                sharedQueue.consume();
+                Thread.sleep(1000);
+            }
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+}
+
+public class ProducerConsumerSimulation {
+    public static void main(String[] args) {
+        SharedQueue sharedQueue = new SharedQueue(5);
+
+        Thread producerThread = new Thread(new Producer(sharedQueue));
+        Thread consumerThread = new Thread(new Consumer(sharedQueue));
+
+        producerThread.start();
+        consumerThread.start();
+    }
+}
